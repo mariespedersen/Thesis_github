5ebd3019741b2e210785c0c3fca0d5ce33b5677a
Marie Sjørslev Pedersen <marie.sp.1996@gmail.com>
2023-02-10 10:14:31 +0100

    added the setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import matplotlib.cm as cm
+import requests
+import json
+import pandas as pd
+import time
+import datetime
+import os
+from unidecode import unidecode
+import sys
+import glob
+import re
+import math
+import random
+import pickle
+import copy
+import itertools
+import collections
+import warnings
+warnings.filterwarnings('ignore')
+from bs4 import BeautifulSoup
